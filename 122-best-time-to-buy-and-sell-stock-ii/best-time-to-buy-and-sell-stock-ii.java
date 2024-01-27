f7a1dd7f2321581f95536dc9f8906d7f3b230aaa
Egor Gordeev <superstep_007@mail.ru>
2024-01-27 11:40:16 +0300

    Time: 1 ms (90.48%) | Memory: 45.7 MB (34.59%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,35 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        
-        int totalProfit = 0;
-        int bPrice = 0;
-        Boolean b = false;
-
-        for(int i = 0; i < prices.length; i++) {
-            int currentPrice = prices[i];
-
-            if(i < prices.length - 1 && currentPrice < prices[i+1] && !b) {
-                bPrice = currentPrice;
-                b = true;
-            } else if (b && currentPrice > bPrice && i < prices.length - 1) {
-                if(currentPrice > prices[i+1]) {
-                    totalProfit += getProfit(bPrice, prices[i]);
-                    bPrice = 0;
-                    b = false;
-                } else if(currentPrice <= prices[i+1]) {
-                    continue;
-                }
-            } else if (b && i == prices.length - 1){
-                totalProfit += getProfit(bPrice, prices[i]);
+        int minVal=Integer.MAX_VALUE;
+        int profit=0;
+        for(int i=0;i<prices.length;i++){//7,1,5,3,6,4
+            if(minVal>prices[i]){
+                minVal=prices[i];//7,1
+            }
+            if(prices[i]>minVal){
+                profit=profit+prices[i]-minVal;
+                minVal=prices[i];
             }
 
         }
-        return totalProfit;
-    }
-
-    private int getProfit(int b, int s) {
-        if(s<b)
-            return 0;
-        return s-b;
+        return profit;
     }
 }
