e186081768664a3428f55c00c64e594c29b7df9e
Egor Gordeev <superstep_007@mail.ru>
2024-02-04 10:18:18 +0300

    Time: 0 ms (100.00%) | Memory: 41.5 MB (41.65%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,43 +1,22 @@
 class Solution {
-    public boolean isValid(String string) {
-         Stack<Character> stack = new Stack<>();
+    public boolean isValid(String str) {
+        int i = -1;
+        char[] stack = new char[str.length()];
 
-        for (char c : string.toCharArray()) {
-
-            try {
-                switch (c) {
-                    case 40, 91, 123:
-                        stack.push(c);
-                        break;
-                    case 41: {
-                        char res = stack.pop();
-                        if (res != 40) {
-                            return false;
-                        }
-                    }
-                    break;
-                    case 93: {
-                        char res = stack.pop();
-                        if (res != 91) {
-                            return false;
-                        }
-                    }
-                    break;
-                    case 125: {
-                        char res = stack.pop();
-                        if (res != 123) {
-                            return false;
-                        }
-                    }
-                    break;
+        for (char ch : str.toCharArray()) {
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack[++i] = ch;
+            } else {
+                if (i >= 0 &&
+                    ((stack[i] == '(' && ch == ')') ||
+                    (stack[i] == '[' && ch == ']') ||
+                    (stack[i] == '{' && ch == '}'))) {
+                    i--;
+                } else {
+                    return false;
                 }
-            } catch (Exception e) {
-                return false;
             }
-
-
         }
-
-        return stack.empty();
+        return i == -1;
     }
 }
