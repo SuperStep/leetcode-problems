ec0c5af73ac0d0d4cb5fba7dad77f299a9a42419
Egor Gordeev <superstep_007@mail.ru>
2024-02-07 18:26:37 +0300

    Time: 1462 ms (18.28%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,18 +1,13 @@
-select p.name as person_name
-from 
-(
-    select q.person_id as person_id, q.turn as turn, 
-    sum ( q.weight ) over ( 
-         order by q.turn 
-         rows unbounded preceding 
-       ) prev_w 
-    
-from Queue as q
-order by q.turn
-) as q
-left join (select distinct  person_id, person_name as name from Queue) as p
-on q.person_id = p.person_id
-group by q.person_id, q.turn, p.name
-having max(q.prev_w) <= 1000
-order by q.turn desc 
-limit 1
+  select
+        person_name
+    from (
+        select
+            person_name, 
+            turn,
+            sum(weight) over (order by turn) as total_weight
+        from Queue
+        order by turn desc
+    ) a
+    where
+        total_weight <= 1000
+    limit 1
