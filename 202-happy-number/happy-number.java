201855a2a8247c65f95d7e3b25f705c0b8dcd17c
Egor Gordeev <superstep_007@mail.ru>
2024-02-19 22:28:46 +0300

    Time: 0 ms (100.00%) | Memory: 40.3 MB (93.74%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -1,21 +1,17 @@
 class Solution {
     public boolean isHappy(int n) {
-        Set<Integer> seen = new HashSet<>();
-        while (n != 1 && !seen.contains(n)) {
-            seen.add(n);
-            n = sumOfSquaresOfDigits(n);
-        }
-        return n == 1;
+        int sum=0;
+       while(n>0)
+       {
+          int d=n%10;
+           sum=sum+d*d;
+           n=n/10;
+       } 
+      n=sum;
+     if(n>9)
+       {return isHappy(n);}
+       else if(n<=9 && n!=1 && n!=7)
+       {return false;}
+       return true;
     }
-
-    private static int sumOfSquaresOfDigits(int n) {
-        int sum = 0;
-        while (n > 0) {
-            int digit = n % 10;
-            sum += digit * digit;
-            n /= 10;
-        }
-        return sum;
-    }
-
 }
