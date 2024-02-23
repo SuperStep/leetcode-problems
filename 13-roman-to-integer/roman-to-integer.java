275ec6a3ba51f4971545cf62603a390c5cbab284
Egor Gordeev <superstep_007@mail.ru>
2024-02-23 09:41:34 +0300

    Time: 2 ms (100.00%) | Memory: 44.1 MB (98.32%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.java
+++ 13-roman-to-integer/roman-to-integer.java
@@ -1,29 +1,21 @@
 class Solution {
     public int romanToInt(String s) {
-        // Create a HashMap to store the values of each Roman numeral
-        HashMap<Character, Integer> map = new HashMap<>();
-        map.put('I', 1);
-        map.put('V', 5);
-        map.put('X', 10);
-        map.put('L', 50);
-        map.put('C', 100);
-        map.put('D', 500);
-        map.put('M', 1000);
-
-        int total = 0;
-        int length = s.length();
-        
-        // Iterate through the Roman numeral string from left to right
-        for (int i = 0; i < length; i++) {
-            int currentValue = map.get(s.charAt(i));
-            // If the value of the numeral is less than the value of the next numeral, subtract it
-            if (i < length - 1 && currentValue < map.get(s.charAt(i + 1))) {
-                total -= currentValue;
-            } else {
-                total += currentValue;
+        int ans = 0, num = 0;
+        for(int i = s.length()-1;i>=0;i--){
+            switch(s.charAt(i)){
+                case 'I': num = 1;break;
+                case 'V': num = 5;break;
+                case 'X': num = 10;break;
+                case'L': num = 50;break;
+                case'C': num = 100;break;
+                case'D': num = 500;break;
+                case'M': num = 1000; break;
             }
-        }
-        
-        return total;
+            if(4 * num < ans)
+                ans -= num;
+            else 
+                ans+= num;
+        } 
+        return ans;
     }
 }
