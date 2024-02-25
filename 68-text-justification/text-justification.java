4037349ae4562dbf0ed680034197c31b226e5529
Egor Gordeev <superstep_007@mail.ru>
2024-02-25 10:52:21 +0300

    Time: 0 ms (100.00%) | Memory: 42.1 MB (12.79%) - LeetSync

--- 68-text-justification/text-justification.java
+++ 68-text-justification/text-justification.java
@@ -1,48 +1,68 @@
 class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) {
-        List<String> result = new ArrayList<>();
-        List<String> lineWords = new ArrayList<>();
-        int lineLength = 0;
-
         for (String word : words) {
-            if (lineLength + word.length() + lineWords.size() > maxWidth) {
-                // Justify the current line
-                int spaces = maxWidth - lineLength;
-                if (lineWords.size() == 1) {
-                    result.add(lineWords.get(0) + " ".repeat(spaces));
-                } else {
-                    int gaps = lineWords.size() - 1;
-                    int spacesBetweenWords = spaces / gaps;
-                    int extraSpaces = spaces % gaps;
-                    StringBuilder justifiedLine = new StringBuilder();
-                    for (int i = 0; i < gaps; i++) {
-                        justifiedLine.append(lineWords.get(i));
-                        justifiedLine.append(" ".repeat(spacesBetweenWords + (i < extraSpaces ? 1 : 0)));
-                    }
-                    justifiedLine.append(lineWords.get(lineWords.size() - 1));
-                    result.add(justifiedLine.toString());
-                }
-
-                // Start a new line
-                lineWords.clear();
-                lineWords.add(word);
-                lineLength = word.length();
-            } else {
-                lineWords.add(word);
-                lineLength += word.length();
+            if (word.length() > maxWidth) {
+                throw new IllegalArgumentException();
             }
         }
-
-        // Justify the last line (left-justified)
-        StringBuilder lastLine = new StringBuilder();
-        for (String word : lineWords) {
-            lastLine.append(word);
-            lastLine.append(" ");
+        List<String> lines = new ArrayList<>();
+        int firstWordInLine = 0;
+        int unjustifiedLineLength = words[firstWordInLine].length();
+        for (int i = 1; i < words.length; i++) {
+            String word = words[i];
+            int wordLength = word.length();
+            if (unjustifiedLineLength + wordLength + 1 <= maxWidth) { // line has space
+                unjustifiedLineLength += wordLength + 1; // + 1 for space
+            } else { // line has no more space
+                int lastWordInLine = i - 1;
+                lines.add(makeLine(words, maxWidth, firstWordInLine, lastWordInLine,
+                    unjustifiedLineLength));
+                firstWordInLine = i;
+                unjustifiedLineLength = wordLength;
+            }
         }
-        lastLine.deleteCharAt(lastLine.length() - 1); // Remove the trailing space
-        lastLine.append(" ".repeat(maxWidth - lastLine.length()));
-        result.add(lastLine.toString());
+        lines.add(makeLine(words, maxWidth, firstWordInLine, words.length - 1, unjustifiedLineLength));
+        return lines;
+    }
 
-        return result;
+    private String makeLine(String[] words, 
+                            int maxWidth, 
+                            int firstWordInLine, 
+                            int lastWordInLine,
+                            int unjustifiedLineLength) {
+        StringBuilder sb = new StringBuilder(maxWidth);
+        sb.append(words[firstWordInLine]);
+        if (lastWordInLine == words.length - 1) {
+            for (int i = firstWordInLine + 1; i <= lastWordInLine; i++) {
+                sb.append(" "); // always before a word
+                sb.append(words[i]);
+            }
+            while (sb.length() < maxWidth) {
+                sb.append(" ");
+            }
+            return sb.toString();
+        }
+        int totalWords = lastWordInLine - firstWordInLine + 1;
+        if (totalWords == 1) {
+            while (sb.length() < maxWidth) {
+                sb.append(" ");
+            }
+            return sb.toString();
+        }
+        int additionalSpaces = maxWidth - unjustifiedLineLength;
+        int additionalSpacesPerWord = additionalSpaces / (totalWords - 1);
+        int leftSpaces = additionalSpaces % (totalWords - 1);
+        for (int i = firstWordInLine + 1; i <= lastWordInLine; i++) {
+            sb.append(" "); // always before a word
+            for (int j = 0; j < additionalSpacesPerWord; j++) {
+                sb.append(" ");
+            }
+            if (leftSpaces > 0) {
+                sb.append(" ");
+                leftSpaces--;
+            }
+            sb.append(words[i]);
+        }
+        return sb.toString();
     }
 }
