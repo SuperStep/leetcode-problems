94ed6fc40e9a078a787cd96b8e94f918586d5a3a
Egor Gordeev <superstep_007@mail.ru>
2024-03-13 19:36:58 +0300

    Time: 0 ms (100.00%) | Memory: 40.3 MB (78.99%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,15 +1,8 @@
 class Solution {
     public int pivotInteger(int n) {
-       long totalSum = n * (n + 1) / 2;
-        long leftSum = 0;
-
-        for (int x = 1; x <= n; x++) {
-            leftSum += x;
-            if (leftSum == totalSum - leftSum + x) {
-                return x;
-            }
-        }
-
-        return -1;
+        int y = n * (n + 1) / 2;
+        int x = (int)Math.sqrt(y);
+        if(x * x == y) return x;
+        else return -1;
     }
 }
