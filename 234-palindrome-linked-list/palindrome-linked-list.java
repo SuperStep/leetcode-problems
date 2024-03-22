d8a044d7f2a6133085eb6d4b9355b73e8e0619b7
Egor Gordeev <superstep_007@mail.ru>
2024-03-22 08:41:06 +0300

    Time: 2 ms (100.00%) | Memory: 69.8 MB (6.61%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.java
+++ 234-palindrome-linked-list/palindrome-linked-list.java
@@ -1,59 +1,24 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
+    private static final int[] array = new int[100_000];
     public boolean isPalindrome(ListNode head) {
-        if (head == null || head.next == null) {
+        if(head==null || head.next==null){
             return true;
         }
+       ListNode h= head;
+       final int[] array1 = array;
+       int size =0;
 
-        // Find the end of the first half and reverse the second half.
-        ListNode firstHalfEnd = endOfFirstHalf(head);
-        ListNode secondHalfStart = reverseList(firstHalfEnd.next);
+       while(h!=null){
+           array1[size++] = h.val;
+           h=h.next;
+       }
 
-        // Check whether or not there is a palindrome.
-        ListNode p1 = head;
-        ListNode p2 = secondHalfStart;
-        boolean result = true;
-        while (result && p2 != null) {
-            if (p1.val != p2.val) result = false;
-            p1 = p1.next;
-            p2 = p2.next;
-        }
-
-        // Restore the list and return the result.
-        firstHalfEnd.next = reverseList(secondHalfStart);
-        return result;
-    }
-
-    // Helper function to reverse the second half of the list.
-    private ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-        while (curr != null) {
-            ListNode nextTemp = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = nextTemp;
-        }
-        return prev;
-    }
-
-    // Helper function to find the end of the first half of the list.
-    private ListNode endOfFirstHalf(ListNode head) {
-        ListNode fast = head;
-        ListNode slow = head;
-        while (fast.next != null && fast.next.next != null) {
-            fast = fast.next.next;
-            slow = slow.next;
-        }
-        return slow;
+       int m = size/2;
+       for(int i=0;i<m;i++){
+           if(array[i] != array1[--size]){
+               return false;
+           }
+       }
+       return true;
     }
 }
