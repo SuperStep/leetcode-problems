e1aee5e6ceff120cf31d8bf012b1c4f5d22fc1ba
Egor Gordeev <superstep_007@mail.ru>
2024-03-26 08:29:40 +0300

    Time: 1 ms (98.00%) | Memory: 54.9 MB (73.20%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,41 +1,18 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
+        boolean[] found = new boolean[n+1];
+        for(int i = 0;i< n; i++){
+            if (nums[i]>0 && nums[i]<= n){
+                found[nums[i]]=true;
 
-        // First, separate positive and negative numbers.
-        int shift = segregate(nums, n);
-
-        // Use the array index as a hash to store the frequency of positive numbers.
-        int[] positiveNums = new int[n - shift];
-        for (int i = shift; i < n; ++i) {
-            int num = Math.abs(nums[i]);
-            if (num <= n - shift) {
-                positiveNums[num - 1] = nums[i] > 0 ? -nums[i] : nums[i];
             }
         }
-
-        // Find the first index which has a positive value.
-        for (int i = 0; i < positiveNums.length; ++i) {
-            if (positiveNums[i] >= 0) {
-                return i + 1;
+        for(int i= 1;i<=n;i++){
+            if(!found[i]){
+                return i;
             }
         }
-
-        // If all indices are marked, return the next number.
-        return positiveNums.length + 1;
-    }
-
-     // Function to segregate positive and negative numbers.
-    private int segregate(int[] nums, int size) {
-        int j = 0;
-        for (int i = 0; i < size; i++) {
-            if (nums[i] <= 0) {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-                j++;
-            }
-        }
-        return j;
-    }
+        return n+1;
+ }
 }
