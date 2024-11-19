17b86e5a994c9ca10c5f0bfb43afeb8a0e6373d8
Egor Gordeev <superstep_007@mail.ru>
2024-11-19 20:40:18 +0300

    Time: 146 ms (69.81%) | Memory: 34.3 MB (81.72%) - LeetSync

--- 1894-merge-strings-alternately/merge-strings-alternately.kt
+++ 1894-merge-strings-alternately/merge-strings-alternately.kt
@@ -1,10 +1,13 @@
 class Solution {
-    fun mergeAlternately(word1: String, word2: String): String = 
-        (0 until maxOf(word1.length, word2.length))
-            .joinToString("") { i ->
-                buildString {
-                    if (i < word1.length) append(word1[i])
-                    if (i < word2.length) append(word2[i])
-                }
-            }
+    fun mergeAlternately(word1: String, word2: String): String {
+        val result = StringBuilder()
+        val maxLength = maxOf(word1.length, word2.length)
+        
+        for (i in 0 until maxLength) {
+            if (i < word1.length) result.append(word1[i])
+            if (i < word2.length) result.append(word2[i])
+        }
+        
+        return result.toString()
+    }
 }
