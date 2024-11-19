2b10aae9588fe027dbdb8f4e81087ec041fc5e24
Egor Gordeev <superstep_007@mail.ru>
2024-11-19 20:39:34 +0300

    Time: 160 ms (36.47%) | Memory: 34.5 MB (61.34%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun mergeAlternately(word1: String, word2: String): String {
+        val result = StringBuilder()
+        val maxLength = maxOf(word1.length, word2.length)
+        
+        for (i in 0 until maxLength) {
+            if (i < word1.length) result.append(word1[i])
+            if (i < word2.length) result.append(word2[i])
+        }
+        
+        return result.toString()
+    }
+}
