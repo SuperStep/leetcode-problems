d2842e89b809cdc06ba09dbc27ec569279074617
Egor Gordeev <superstep_007@mail.ru>
2025-06-16 22:18:53 +0300

    Time: 0 ms (100.00%) | Memory: 41.9 MB (48.40%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,6 +1,9 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        s = s.stripTrailing();
-        return s.length() - 1 - s.lastIndexOf(" ");
+        s = s.trim();
+        if(s.length() == 0) return 0;
+
+        int lastIndexOfSpace = s.lastIndexOf(' ');
+        return s.length() - lastIndexOfSpace - 1;
     }
 }
